fe2516386f41842282ef95888b56f311da4a4e8f
Faraz786786786 <153177633+Faraz786786786@users.noreply.github.com>
2025-05-20 22:31:05 +0530

    Update Login.jsx

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
          
          if (state === 'Admin') {
           
-          const {data} = await axios.post(backendUrl + 'api/admin/login',{email,password})
+          const {data} = await axios.post(backendUrl + '/api/admin/login',{email,password})
           if(data.success){
             localStorage.setItem('aToken',data.token)
             setAToken(data.token);
